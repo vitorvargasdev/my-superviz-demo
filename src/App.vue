eca58b591ae14cb9b69c77df8723a41ddc7b4c99
vitorvargasdev <instalando@protonmail.com>
2024-04-22 11:51:02 -0300

    feat: add support to realtime

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,22 @@
 import { onMounted, ref } from 'vue'
 import SuperVizRoom from "../../sdk/dist";
 import { VideoConference } from "../../sdk/dist";
-import { CanvasPin, Comments } from "../../sdk/dist";
+import { CanvasPin, Comments, Realtime } from "../../sdk/dist";
 
 const DEVELOPER_KEY = import.meta.env.VITE_DEVELOPER_API_KEY;
 
+const urlParams = new URLSearchParams(window.location.search);
+
 const room: any = ref(null)
+const userId = urlParams.get("userId") || "<USER-ID>";
+const userName = urlParams.get("userName") || "<USER-NAME>";
+const channels = ref<any>([]);
+
+const config = {
+  video: false,
+  comments: false,
+  realtime: true
+}
 
 const initializeSuperVizRoom = async () => {
   room.value = await SuperVizRoom(DEVELOPER_KEY, {
@@ -16,8 +27,8 @@ const initializeSuperVizRoom = async () => {
       name: "<GROUP-NAME>",
     },
     participant: {
-      id: "<USER-ID>",
-      name: "Vitor",
+      id: userId,
+      name: userName,
       avatar: {
         "imageUrl": "https://<PATH>",
         "model3DUrl": "https://<PATH>",
@@ -28,12 +39,14 @@ const initializeSuperVizRoom = async () => {
 }
 
 const initComments = async () => {
+  if (!config.comments) return
   const pinAdapter = new CanvasPin("my-id");
   const comments = new Comments(pinAdapter);
   await room.value.addComponent(comments);
 } 
 
 const initVideo = async () => {
+  if (!config.video) return
   const video = new VideoConference({
     camsOff: false,
     chatOff: false,
@@ -57,16 +70,56 @@ const initVideo = async () => {
   await room.value.addComponent(video);
 }
 
+const initRealtime = async () => {
+  if (!config.realtime) return
+
+  const realtime = new Realtime();
+  await room.value.addComponent(realtime);
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+
+  channels.value = [
+    realtime.connect("channel-1"),
+    realtime.connect("channel-2"),
+    realtime.connect("channel-3"),
+  ]
+
+  console.log("Channels: ", channels.value)
+
+  channels.value[0].subscribe("my-event", (data: any) => {
+    speakEvent(data, 0);
+  });
+
+  channels.value[1].subscribe("my-event", (data: any) => {
+    speakEvent(data, 1);
+  });
+
+  channels.value[2].subscribe("my-event", (data: any) => {
+    speakEvent(data, 2);
+  });
+}
+
+const speakEvent = (data: any, channelId: number) => {
+  console.log(`Hey, we received one event on channel ${channelId}: `, data);
+}
+
+const emitEvent = (channelId: number) => {
+  channels.value[channelId].publish("my-event", { message: "Hey! This message is coming from channel: " + channelId + "!" });
+}
+
 onMounted(async () => {
   await initializeSuperVizRoom();
   await initVideo();
   await initComments();
+  await initRealtime();
 })
 
 </script>
 
 <template>
   <div>
+    <button @click="emitEvent(0)">Emit Event to channel 1</button>
+    <button @click="emitEvent(1)">Emit Event to channel 2</button>
+    <button @click="emitEvent(2)">Emit Event to channel 3</button>
    <canvas id="my-id" width="540" height="540"></canvas> 
   </div>
 </template>
